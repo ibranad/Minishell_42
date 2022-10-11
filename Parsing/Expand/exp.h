edb87912885136f3ee90369044c662a422f5914d
Ibrahim Nada <57199774+ibranad@users.noreply.github.com>
2022-10-11 19:36:12 +0100

    expanding mostly normed

--- Parsing/Expand/exp.h
+++ Parsing/Expand/exp.h
@@ -6,7 +6,7 @@
 /*   By: ibnada <ibnada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 17:29:34 by ibnada            #+#    #+#             */
-/*   Updated: 2022/10/10 20:09:02 by ibnada           ###   ########.fr       */
+/*   Updated: 2022/10/11 18:35:11 by ibnada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 #define EXP_H
 
 #include <stdio.h>
-#include <string.h>
+//#include <string.h>
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <ctype.h>
+//#include <ctype.h>
 
 #define SQ '\'' // Single Quote
 #define DQ '\"' // Double Quote
@@ -32,9 +32,35 @@ typedef struct exp_s
     int g_i;
 }   t_exp;
 
-char *get_until_dollar(char *in);
-char *get_until_s_quote(char *in);
-char *get_until_d_quote(char *in);
+typedef struct exp_sp
+{
+    char *out;
+    char *not_expa;
+    char *expa;
+    int i_g;
+}   t_exp_sp;
 
+int     ft_isalnum(int c);
+int	    min(int a, int b);
+int     ft_isdigit(int c);
+char    *d_quote_sp(char *in);
+void    struct_init(t_exp *s);
+int     ft_strlen(char const *str);
+char	*ft_strdup(const char *s1);
+void    struct_fill_sp(t_exp_sp *p);
+char    *get_until_dollar(char *in);
+void    struct_fill_sp(t_exp_sp *p);
+char    *get_until_s_quote(char *in);
+char    *get_until_d_quote(char *in);
+void    sq_expanding(t_exp *s, char *in);
+void    dq_expanding(t_exp *s, char *in);
+void    dollar_expanding(t_exp *s, char *in);
+void    dollar_expanding_sp(t_exp_sp *p, char *in);
+void    dollar_expanding_sp(t_exp_sp *p, char *in);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char    *expand_dq_sp(char *in);
+int     unclosed_quote(char *in);
 
 #endif
--- Parsing/Expand/exp_util_3.c
+++ Parsing/Expand/exp_util_3.c
@@ -6,7 +6,7 @@
 /*   By: ibnada <ibnada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 20:23:01 by ibnada            #+#    #+#             */
-/*   Updated: 2022/10/10 20:25:30 by ibnada           ###   ########.fr       */
+/*   Updated: 2022/10/11 17:56:51 by ibnada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,77 @@
 
 void dollar_expanding(t_exp *s, char *in)
 {
-    if (isdigit(in[s->g_i + 1]))
+    char *ptr;
+
+    ptr = NULL;
+    if (ft_isdigit(in[s->g_i + 1]))
         {
             s->g_i++;
+            ptr = s->out;
             if (in[s->g_i] == '0')
+            {
                 s->out = ft_strjoin(s->out, "Minishell");
+                free(ptr);
+            }
+            ptr = s->out;
             s->expa = get_until_dollar(&in[s->g_i + 1]);
             s->out = ft_strjoin(s->out, s->expa);
+            free(ptr);
+            s->g_i += ft_strlen(s->expa) + 1;
             free(s->expa);
-            s->g_i += ft_strlen(s->expa);
         }
         else
         {
+            ptr = s->out;
             s->expa = get_until_dollar(&in[s->g_i + 1]);
             s->out = ft_strjoin(s->out, getenv(s->expa));
-            free(s->expa);
+            free(ptr);
             s->g_i += ft_strlen(s->expa) + 1;
+            free(s->expa);
         }
 }
 
 void dq_expanding(t_exp *s, char *in)
 {
     //This sub_function is used for norm purpose
+    char *ptr;
+    char *ptr2;
+
+    ptr = NULL;
     s->g_i++;
+    ptr = s->out;
     s->expa = get_until_d_quote(&in[s->g_i]);
-    s->out = ft_strjoin(s->out, expand_dq_sp(s->expa));
-    free(s->expa);
+    ptr2 = expand_dq_sp(s->expa);
+    s->out = ft_strjoin(s->out, ptr2);
+    free(ptr);
+    free(ptr2);
     s->g_i += ft_strlen(s->expa) + 1;
+    free(s->expa);
 }
 
 void sq_expanding(t_exp *s, char *in)
 {
     //This sub_function is used for norm purpose
+    char *ptr;
+    
+    ptr = NULL;
     s->g_i++;
+    ptr = s->out;
     s->not_out = get_until_s_quote(&in[s->g_i]);
     s->out = ft_strjoin(s->out, s->not_out);
-    free(s->not_out);
+    free(ptr);
     s->g_i += ft_strlen(s->not_out) + 1;
+    free(s->not_out);
+}
+
+int ft_strlen(char const *str)
+{
+    int i;
+
+    if (!str)
+        return (0);
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
 }
