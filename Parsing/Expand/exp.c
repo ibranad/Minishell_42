e7bce7433f4b41ff413e78939ef2aee59f640a03
Ibrahim Nada <57199774+ibranad@users.noreply.github.com>
2022-10-12 20:30:57 +0100

    expanding normed

--- Parsing/Expand/exp.c
+++ Parsing/Expand/exp.c
@@ -6,54 +6,16 @@
 /*   By: ibnada <ibnada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 17:57:25 by ibnada            #+#    #+#             */
-/*   Updated: 2022/10/11 19:15:43 by ibnada           ###   ########.fr       */
+/*   Updated: 2022/10/12 19:16:15 by ibnada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exp.h"
 
-// char *expand_dq_sp(char *in)
-// {
-//     char *out = NULL;
-//     char *not_expa = NULL;
-//     char *expa = NULL;
-//     int i;
-
-//     i = 0;
-//     while (in[i])
-//     {
-//         if (in[i] == '$')
-//         {
-//             if (ft_isdigit(in[i + 1]))
-//             {
-//                 i++;
-//                 if (in[i] == '0')
-//                     out = ft_strjoin(out, "Minishell");
-//                 expa = get_until_dollar(&in[i + 1]);
-//                 out = ft_strjoin(out, expa);
-//                 i += ft_strlen(expa);
-//             }
-//             else
-//             {
-//                 expa = get_until_dollar(&in[i + 1]);
-//                 out = ft_strjoin(out, getenv(expa));
-//                 i += ft_strlen(expa) + 1;
-//             }
-//         }
-//         else
-//         {
-//             not_expa = get_until_dollar(&in[i]);
-//             out = ft_strjoin(out, not_expa);
-//             i += ft_strlen(not_expa);
-//         }
-//     }
-//     return (out);
-// }
-
-char *expand_var_in_str(char *in)
-{
     //expand env variable in any given string
     //for example input:echo $USER$USER -> output:ibnadaibnada
+char *expand_var_in_str(char *in)
+{
     t_exp s;
     char *ptr;
 
@@ -62,34 +24,25 @@ char *expand_var_in_str(char *in)
     while (in[s.g_i])
     {
         if (in[s.g_i] == '$' && !in[s.g_i + 1])
-        {
-            s.out = ft_strjoin(s.out, "$");
-            s.g_i += 1;
-        }
-        if (in[s.g_i] == '$')
+            dollar_only_case(&s, in);
+        else if (in[s.g_i] == '$')
             dollar_expanding(&s,in);
         else if (in[s.g_i] == '\"')
             dq_expanding(&s, in);
         else if (in[s.g_i] == '\'')
             sq_expanding(&s, in);
         else
-        {
-            ptr = s.out;
-            s.not_out = get_until_dollar(&in[s.g_i]);
-            s.out = ft_strjoin(ptr, s.not_out);
-            free(ptr);
-            s.g_i += ft_strlen(s.not_out);
-            free(s.not_out);
-        }
+            exp_else(&s, in);
     }
     return (s.out);
 }
 
 int check_unrequired_by_subject(char *in)
 {
+    //error code -1: unclosed quote
     if (unclosed_quote(in) == 1)
-        return(-1);//error code -1: unclosed quote
-    if ()
+        return(-1);
+    return (0);
 }
 
 int main(int ac, char *av[], char *env[])
--- Parsing/Expand/exp_util_7.c
+++ Parsing/Expand/exp_util_7.c
@@ -1,57 +1,57 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exp_util_3.c                                       :+:      :+:    :+:   */
+/*   exp_util_7.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ibnada <ibnada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/10 20:23:01 by ibnada            #+#    #+#             */
-/*   Updated: 2022/10/12 17:41:57 by ibnada           ###   ########.fr       */
+/*   Created: 2022/10/12 16:51:42 by ibnada            #+#    #+#             */
+/*   Updated: 2022/10/12 18:35:18 by ibnada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exp.h"
 
-void dollar_expanding(t_exp *s, char *in)
+void    dollar_only_case(t_exp *s, char *in)
 {
     char *ptr;
 
     ptr = NULL;
-    if (ft_isdigit(in[s->g_i + 1]))
-        dollar_expanding_if_short(s, in);
-    else
-        dollar_expanding_else_short(s, in);
+    ptr = s->out;
+    s->out = ft_strjoin(s->out, "$");
+    s->g_i += 1;
+    free(ptr);
 }
 
-void dollar_expanding_if_short(t_exp *s, char *in)
+void dq_expanding(t_exp *s, char *in)
 {
+    //This sub_function is used for norm purpose
     char *ptr;
+    char *ptr2;
 
     ptr = NULL;
     s->g_i++;
     ptr = s->out;
-    if (in[s->g_i] == '0')
-    {
-        s->out = ft_strjoin(s->out, "Minishell");
-        free(ptr);
-        ptr = s->out;
-    }
-    s->expa = get_until_dollar(&in[s->g_i + 1]);
-    s->out = ft_strjoin(s->out, s->expa);
+    s->expa = get_until_d_quote(&in[s->g_i]);
+    ptr2 = expand_dq_sp(s->expa);
+    s->out = ft_strjoin(s->out, ptr2);
     free(ptr);
+    free(ptr2);
     s->g_i += ft_strlen(s->expa) + 1;
     free(s->expa);
 }
 
-void dollar_expanding_else_short(t_exp *s, char *in)
+void sq_expanding(t_exp *s, char *in)
 {
+    //This sub_function is used for norm purpose
     char *ptr;
-
+    
     ptr = NULL;
+    s->g_i++;
     ptr = s->out;
-    s->expa = get_until_dollar(&in[s->g_i + 1]);
-    s->out = ft_strjoin(s->out, getenv(s->expa));
+    s->not_out = get_until_s_quote(&in[s->g_i]);
+    s->out = ft_strjoin(s->out, s->not_out);
     free(ptr);
-    s->g_i += ft_strlen(s->expa) + 1;
-    free(s->expa);
+    s->g_i += ft_strlen(s->not_out) + 1;
+    free(s->not_out);
 }
