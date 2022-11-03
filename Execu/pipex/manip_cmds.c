c64f2c14439bbb6f2ef7827d22c063c2642d4c61
Oussama Bouizgarne <obouizga@student.1337.ma>
2022-11-03 14:34:41 +0100

    Fix : redirections in pipelilne disfunctioned due to a typo

--- Execu/pipex/manip_cmds.c
+++ Execu/pipex/manip_cmds.c
@@ -6,7 +6,7 @@
 /*   By: obouizga <obouizga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 10:37:41 by obouizga          #+#    #+#             */
-/*   Updated: 2022/11/03 08:56:27 by obouizga         ###   ########.fr       */
+/*   Updated: 2022/11/03 14:14:49 by obouizga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,35 +22,35 @@ void	run(t_cmdl *cmd, int cmdline_type, char **env)
 
 void	first_cmd(int *fildes, t_cmdl *cmd, int validity, char **env)
 {
+	read_from(cmd->in_fd);
+	write_to_pipe(fildes);
 	if (validity == _builtin_)
 		run_builtin(cmd, PIPELINE);
 	else if (validity == _unset_path_ ||\
 			 validity == _command_not_found_)
 		exit(127);
-	read_from(cmd->in_fd);
-	write_to_pipe(fildes);
 	run(cmd, PIPELINE, env);
 }
 
 void	mid_cmd(int *fildes, t_cmdl *cmd, int validity, char **env)
 {
+	write_to_pipe(fildes);
 	if (validity == _builtin_)
 		run_builtin(cmd, PIPELINE);
 	else if (validity == _unset_path_ ||\
 			 validity == _command_not_found_)
 		exit(127);
-	write_to_pipe(fildes);
 	run(cmd, PIPELINE, env);
 }
 
 void	last_cmd(t_cmdl *cmd, int validity, char **env)
 {
+	write_to(cmd->out_fd);
 	if (validity == _builtin_)
 		run_builtin(cmd, PIPELINE);
 	else if (validity == _unset_path_ ||\
 			 validity == _command_not_found_)
 		exit(127);
-	write_to(cmd->out_fd);
 	run(cmd, PIPELINE, env);
 }
 	
