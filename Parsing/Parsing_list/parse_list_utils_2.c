5a51d10704ee3d93d9c57c36eae129f9eac3ee7a
Ibrahim Nada <57199774+ibranad@users.noreply.github.com>
2022-11-08 18:49:39 +0100

    Norm: parsing

--- Parsing/Parsing_list/parse_list_utils_2.c
+++ Parsing/Parsing_list/parse_list_utils_2.c
@@ -6,32 +6,92 @@
 /*   By: ibnada <ibnada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 09:51:03 by ibnada            #+#    #+#             */
-/*   Updated: 2022/10/27 19:46:10 by ibnada           ###   ########.fr       */
+/*   Updated: 2022/11/08 18:47:40 by ibnada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../../Header/minishell.h"
 
-char *char_at_start_end(char *in , char c)
+char	*char_at_start_end(char *in, char c)
 {
-    int i;
-    int j;
-    char *out;
+	int		i;
+	int		j;
+	char	*out;
 
-    if (!in)
-        return (NULL);
-    i = 0;
-    j = 1;
-    out = malloc(sizeof(char) * ft_strlen(in) + 3);
-    out[0] = c;
-    while(in[i])
-    {
-        out[j] = in[i];
-        i++;
-        j++;
-    }
-    out[j++] = c;
-    out [j] = '\0';
-    return (out);
+	if (!in)
+		return (NULL);
+	i = 0;
+	j = 1;
+	out = malloc(sizeof(char) * ft_strlen(in) + 3);
+	out[0] = c;
+	while (in[i])
+	{
+		out[j] = in[i];
+		i++;
+		j++;
+	}
+	out[j++] = c;
+	out [j] = '\0';
+	return (out);
+}
+
+void	error_printing(void)
+{
+	putstr_fd("Minishell: Syntax error near", 2);
+	putstr_fd(" unexpected token\n", 2);
+}
+
+void	t_prs_lst_init(t_prs_lst *p, t_toklist *tok_lst, t_envl *envl)
+{
+	p->i = 0;
+	p->size = toklist_size_2alloc(tok_lst);
+	p->apnd_flag = 0;
+	p->red_in_flag = 0;
+	p->out_flag = 0;
+	p->red_out_flag = 0;
+	p->here_doc_flag = 0;
+	p->first_word = 0;
+	p->cmd_c = 0;
+	p->paths = get_paths(envl);
+	p->lst = create_parse_lst(p->size);
+	lst_init(&p->lst);
+	p->tmp_2 = p->lst;
+	p->tmp = tok_lst;
+	p->in_flag = -1;
+}
+
+int	toklist_size_2alloc(t_toklist *tok_list)
+{
+	int			i;
+	t_toklist	*tmp;
+
+	i = 0;
+	tmp = tok_list;
+	while (tmp)
+	{
+		if (tmp->nature == _pipe)
+			i++;
+		tmp = tmp->next;
+	}
+	return (i + 1);
+}
+
+void	lst_init(t_cmdl **lst)
+{
+	t_cmdl	*tmp;
+	int		i;
+
+	i = 0;
+	tmp = *lst;
+	while (tmp)
+	{
+		tmp->args = 0;
+		tmp->builtin = -1;
+		tmp->idx = i;
+		tmp->in_fd = 0;
+		tmp->out_fd = 1;
+		tmp->path = 0;
+		i++;
+		tmp = tmp->next;
+	}
 }
--- Parsing/Parsing_list/parse_list_utils_5.c
+++ Parsing/Parsing_list/parse_list_utils_5.c
@@ -6,98 +6,98 @@
 /*   By: obouizga <obouizga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 22:05:23 by ibnada            #+#    #+#             */
-/*   Updated: 2022/11/08 12:18:02 by obouizga         ###   ########.fr       */
+/*   Updated: 2022/11/08 18:14:46 by ibnada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../Header/minishell.h"
 
-int    args_case(t_prs_lst *p)
+int	args_case(t_prs_lst *p)
 {
-    p->tmp_2->args[p->i] = ft_strdup(p->tmp->lexeme);
-    p->i++;
-    if (p->tmp->next)
-    {
-        if (p->tmp->next->nature != _word)
-        {
-            p->tmp_2->args[p->i] = 0;
-            p->tmp = p->tmp->next;
-        }
-        else
-            p->tmp = p->tmp->next;   
-    }
-    else
-    {
-        p->tmp_2->args[p->i] = 0;
-        return (-1);
-    }
-    return (0);
+	p->tmp_2->args[p->i] = ft_strdup(p->tmp->lexeme);
+	p->i++;
+	if (p->tmp->next)
+	{
+		if (p->tmp->next->nature != _word)
+		{
+			p->tmp_2->args[p->i] = 0;
+			p->tmp = p->tmp->next;
+		}
+		else
+			p->tmp = p->tmp->next;
+	}
+	else
+	{
+		p->tmp_2->args[p->i] = 0;
+		return (-1);
+	}
+	return (0);
 }
 
-int here_doc_flag(t_prs_lst *p)
+int	here_doc_flag(t_prs_lst *p)
 {
-    p->here_doc_flag = 1;
-    if (p->tmp->next)
-    {
-        if (is_symbol(p->tmp->next->lexeme[0]))
-            error_printing();
-        p->tmp = p->tmp->next;
-    }
-    else
-    {
-        error_printing();
-        return (-1);
-    } 
-    return (0);
+	p->here_doc_flag = 1;
+	if (p->tmp->next)
+	{
+		if (is_symbol(p->tmp->next->lexeme[0]))
+			error_printing();
+		p->tmp = p->tmp->next;
+	}
+	else
+	{
+		error_printing();
+		return (-1);
+	}
+	return (0);
 }
 
-int here_doc_word(t_prs_lst *p)
+int	here_doc_word(t_prs_lst *p)
 {
-    p->tmp_2->in_fd = ft_heredoc(p->tmp->lexeme);
-    p->here_doc_flag = 0;
-    p->in_flag = 1;
-    if (p->tmp->next)
-    {
-        p->tmp = p->tmp->next;
-        return (0);
-    }
-    else
-        return (-1);
+	p->tmp_2->in_fd = ft_heredoc(p->tmp->lexeme);
+	p->here_doc_flag = 0;
+	p->in_flag = 1;
+	if (p->tmp->next)
+	{
+		p->tmp = p->tmp->next;
+		return (0);
+	}
+	else
+		return (-1);
 }
 
-int input_flag_case(t_prs_lst *p)
+int	input_flag_case(t_prs_lst *p)
 {
-    p->red_in_flag = 1;
-    if (p->tmp->next)
-    {
-        if (is_symbol(p->tmp->next->lexeme[0]))
-            error_printing();
-        p->tmp = p->tmp->next;
-        return(0);
-    }
-    else
-    {
-        error_printing();
-        return (-1);
-    } 
+	p->red_in_flag = 1;
+	if (p->tmp->next)
+	{
+		if (is_symbol(p->tmp->next->lexeme[0]))
+			error_printing();
+		p->tmp = p->tmp->next;
+		return (0);
+	}
+	else
+	{
+		error_printing();
+		return (-1);
+	}
 }
 
-int input_word_case(t_prs_lst *p)
+int	input_word_case(t_prs_lst *p)
 {
-    p->tmp_2->in_fd = open(p->tmp->lexeme, O_RDONLY);
-    p->in_flag = 1;
-    if(p->tmp_2->in_fd < 0)
-    {
-        putstr_fd(strerror(errno), 2);
-        putstr_fd("\n", 2);
-        return (-1);
-    }
-    p->red_in_flag = 0;
-    if (p->tmp->next)
-    {
-        p->tmp = p->tmp->next;
-        return (0);
-    }
-    else
-        return (-1);
+	p->tmp_2->in_fd = open(p->tmp->lexeme, O_RDONLY);
+	p->in_flag = 1;
+	if (p->tmp_2->in_fd < 0)
+	{
+		putstr_fd(strerror(errno), 2);
+		putstr_fd("\n", 2);
+		return (-1);
+	}
+	p->red_in_flag = 0;
+	if (p->tmp->next)
+	{
+		p->tmp = p->tmp->next;
+		return (0);
+	}
+	else
+		return (-1);
 }
