3697a5085b4defad7f95388b197a8961e533f5db
Oussama Bouizgarne <obouizga@student.1337.ma>
2022-11-08 17:37:27 +0100

    Fix : refining UNSET builtin and preventing the SEGV mentioned in #49

--- Execu/builtins/unset_utils.c
+++ Execu/builtins/unset_utils.c
@@ -6,16 +6,16 @@
 /*   By: obouizga <obouizga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 11:02:31 by obouizga          #+#    #+#             */
-/*   Updated: 2022/11/08 11:23:31 by obouizga         ###   ########.fr       */
+/*   Updated: 2022/11/08 15:41:43 by obouizga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../Header/minishell.h"
 
-void	del_one(t_envl *prev, t_envl *node, t_envl *next)
+void	del_one(t_envl *prev, t_envl *target, t_envl *next)
 {
 	prev->next = next;
-	free_envl_n(node);
+	free_envl_n(target);
 }
 
 t_envl	*penult_node(t_envl **envl)
@@ -28,25 +28,40 @@ t_envl	*penult_node(t_envl **envl)
 	return (curr);
 }
 
-void	remove_node(t_envl *curr, t_envl **envl, t_envl *prev)
+// void	remove_node(t_envl *target, t_envl **envl, t_envl *prev)
+// {
+// 	t_envl	*temp;
+// 	t_envl	*penult;
+
+// 	penult = penult_node(envl);
+// 	temp = NULL;
+// 	if (target->idx == 0)
+// 	{
+// 		temp = *envl;
+// 		*envl = (*envl)->next;
+// 		(*envl)->idx = 0;
+// 		free_envl_n(temp);
+// 	}
+// 	else if (!target->next)
+// 	{
+// 		free_envl_n(penult->next);
+// 		penult->next = NULL;
+// 	}
+// 	else
+// 		del_one(prev, target, target->next);
+// }
+
+void	remove_node(t_envl *target, t_envl **envl, t_envl *prev)
 {
 	t_envl	*temp;
-	t_envl	*penult;
 
-	penult = penult_node(envl);
 	temp = NULL;
-	if (curr->idx == 0)
+	if (target == *envl)
 	{
 		temp = *envl;
 		*envl = (*envl)->next;
-		(*envl)->idx = 0;
 		free_envl_n(temp);
 	}
-	else if (curr == penult->next)
-	{
-		free_envl_n(penult->next);
-		penult->next = NULL;
-	}
 	else
-		del_one(prev, curr, curr->next);
+		del_one(prev, target, target->next);
 }
