eed8bbcefab47b558da5cfdac603fea9896d6715
Oussama Bouizgarne <obouizga@student.1337.ma>
2022-11-10 10:26:16 +0100

    Unset refining

--- Execu/builtins/unset_utils.c
+++ Execu/builtins/unset_utils.c
@@ -6,7 +6,7 @@
 /*   By: obouizga <obouizga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 11:02:31 by obouizga          #+#    #+#             */
-/*   Updated: 2022/11/08 15:41:43 by obouizga         ###   ########.fr       */
+/*   Updated: 2022/11/10 10:25:42 by obouizga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,6 @@ void	del_one(t_envl *prev, t_envl *target, t_envl *next)
 	prev->next = next;
 	free_envl_n(target);
 }
-
-t_envl	*penult_node(t_envl **envl)
-{
-	t_envl *curr;
-
-	curr = *envl;
-	while (curr->next->next)
-		curr = curr->next;
-	return (curr);
-}
-
-// void	remove_node(t_envl *target, t_envl **envl, t_envl *prev)
-// {
-// 	t_envl	*temp;
-// 	t_envl	*penult;
-
-// 	penult = penult_node(envl);
-// 	temp = NULL;
-// 	if (target->idx == 0)
-// 	{
-// 		temp = *envl;
-// 		*envl = (*envl)->next;
-// 		(*envl)->idx = 0;
-// 		free_envl_n(temp);
-// 	}
-// 	else if (!target->next)
-// 	{
-// 		free_envl_n(penult->next);
-// 		penult->next = NULL;
-// 	}
-// 	else
-// 		del_one(prev, target, target->next);
-// }
 
 void	remove_node(t_envl *target, t_envl **envl, t_envl *prev)
 {
